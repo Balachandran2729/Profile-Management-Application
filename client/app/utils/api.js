0aea093221613b983dcfa6925cfa4c77fcdd7768
Balachandran <balachandran2729@gmail.com>
2025-09-21 11:57:35 +0530

    change the server url

--- client/app/utils/api.js
+++ client/app/utils/api.js
@@ -1,5 +1,5 @@
 // client/app/utils/api.js
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://profile-management-application-2.onrender.com';
 
 export const fetchProfiles = async () => {
   try {
